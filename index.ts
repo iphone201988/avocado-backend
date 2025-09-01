5763c73524f7ff472483749b9e614837c2383aa4
Inder <iphone201988@gmail.com>
2025-09-01 18:22:43 +0530

    code updated

--- index.ts
+++ index.ts
@@ -43,7 +43,6 @@ const endpointSecret = 'whsec_G75r8cf4gc821pfAw8GZRD86zryv0OKG';
 
 app.use("/uploads/audio", express.static(path.join(__dirname, "uploads/audio")));
 
-app.use(express.json());
 
 
 
@@ -200,6 +199,7 @@ app.post("/webhook", express.raw({ type: "application/json" }), async (req:any, 
 });
 
 
+app.use(express.json());
 
 
 
